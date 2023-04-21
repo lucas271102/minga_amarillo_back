b330c4d0b8904cad51d7d23ca28b40776edad7b2
lucas271102 <lucas001spe@gmail.com>
2023-04-21 12:38:18 -0300

    1 commit backend

---
+++ app.js
@@ -0,0 +1,28 @@
+//let express = require('express');
+import 'dotenv/config.js'
+import express from 'express'
+//let path = require('path');
+import path from 'path'
+//let cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
+//let logger = require('morgan');
+import logger from 'morgan'
+
+//let indexRouter = require('./routes/index');
+import indexRouter from './routes/index.js'
+//let usersRouter = require('./routes/users');
+import usersRouter from './routes/users.js'
+import { __dirname } from './utils.js';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+//var express = require('express');
+import express from 'express'
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+export default router;
