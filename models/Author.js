157cd855fc28ebb4ebf321b2dab0c559e438fba4
Lucas Speziale <103441307+lucas271102@users.noreply.github.com>
2023-04-22 12:36:21 -0300

    b-06 (#6)

--- models/Author.js
+++ models/Author.js
@@ -1,11 +1,11 @@
 import {Schema, Types, model} from "mongoose"
 let schema = new Schema({
 name:{type: String, required:true},
-las_name: {type:String, required:false},
+last_name: {type:String, required:false},
 city:{type:String, required:true},
 country:{type:String, required: true},
 date:{type:Date},
-photo:{String, required: true},
+photo:{type:String, required: true},
 active:{type:Boolean, required:true},
 user_id:{
     type:Types.ObjectId,
@@ -17,5 +17,5 @@ user_id:{
     timestamps:true
 })
 let collection= 'authors'
-let Author= model(schema, collection)
+let Author= model(collection, schema)
 export default Author
